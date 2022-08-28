0abc1fd3300b7cb2f4fbf4135f1314e1312d47f7
fugther <G1570760724@163.com>
2022-08-28 17:02:49 +0800

    增加application.properties配置文件 增加druid连接池

--- src/main/java/com/javaclimb/drug/entity/User.java
+++ src/main/java/com/javaclimb/drug/entity/User.java
@@ -24,4 +24,28 @@ public class User implements Serializable {
 
     /*密码*/
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/test/java/com/javaclimb/drug/DrugApplicationTests.java
+++ src/test/java/com/javaclimb/drug/DrugApplicationTests.java
@@ -1,5 +1,6 @@
 package com.javaclimb.drug;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import com.javaclimb.drug.entity.User;
 import com.javaclimb.drug.service.IUserService;
 import com.javaclimb.drug.service.impl.UserServiceImpl;
@@ -21,5 +22,31 @@ class DrugApplicationTests {
             System.out.println(user1.getPassword());
         }
     }
+
+    /**
+     * druid实现配置中数据库密码加密
+     * @throws Exception
+     */
+    @Test
+    public void druidEncrypt() throws Exception {
+// 密码明文
+        String password = "123456";
+        System.out.println("password " + password);
+        String [] keyPair = ConfigTools.genKeyPair(512);
+        // 私钥
+        String privateKey = keyPair[0];
+        // 公钥
+        String publicKey = keyPair[1];
+        // 用私钥加密后的密文
+        password = ConfigTools.encrypt(privateKey, password);
+        System.out.println("privateKey:"+privateKey);
+
+        System.out.println("publicKey:"+publicKey);
+        // 密文
+        System.out.println("ciphertext:"+password);
+        String decryptPassword = ConfigTools.decrypt(publicKey, password);
+        // 原密码
+        System.out.println("originalPassword" + decryptPassword);
+       }
 
 }
